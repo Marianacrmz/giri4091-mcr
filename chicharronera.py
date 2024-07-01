8d608dba2de15642b3abbc02eec5349bdabbf7e4
marianacrmz <marianacramirez.18@gmail.com>
2024-07-01 16:53:20 -0600

    Chicharronera

---
+++ chicharronera.py
@@ -0,0 +1,9 @@
+a = float(input('Ingresar valor de A'))
+b = float(input('Ingresar valor de B'))
+c = float(input('Ingresar valor de C'))
+
+x1 = (-b + (b*b - 4*a*c)**0.5)/(2*a)
+x2 = (-b - (b*b - 4*a*c)**0.5)/(2*a)
+
+print(x1, ' ',x2)
+
